3b65df92532d5ed49753267ccbe5f8fea6d87db9
Mel Massadian <melmassadian@gmail.com>
2021-08-04 19:01:41 +0200

    Initial Commit

---
+++ SwiftOIIO/SwiftOIIO.h
@@ -0,0 +1,18 @@
+//
+//  SwiftOIIO.h
+//  SwiftOIIO
+//
+//  Created by mel on 04/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftOIIO.
+FOUNDATION_EXPORT double SwiftOIIOVersionNumber;
+
+//! Project version string for SwiftOIIO.
+FOUNDATION_EXPORT const unsigned char SwiftOIIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOIIO/PublicHeader.h>
+
+
